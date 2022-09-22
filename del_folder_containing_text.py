db3ddceca4fe443227a61050aca4c21c68cbe4f0
MD MASBHA UDDIN <masbhanoman@gmail.com>
2022-09-22 18:21:35 +0600

    Update del_folder_containing_text.py

--- del_folder_containing_text.py
+++ del_folder_containing_text.py
@@ -8,7 +8,15 @@ for paths, dirs, files in os.walk(".", topdown = False):
             print(i+1)                   
             shutil.rmtree(os.path.join(paths, name))
      
-
+#import os
+#import sys
+#import shutil
+#rootdir = os.chdir("d:\\Odoo 13.0\\server\\odoo\\odoo13_custom\\odoo15_custom\\")
+#folder_name_text = ''
+#for paths, dirs, files in os.walk("d:\\Odoo 13.0\\server\\odoo\\odoo13_custom\\odoo15_custom\\"):
+#    for name in dirs:
+#        folder_name_text = folder_name_text +' '+ name + '/'
+#print(folder_name_text)
 
  
 
